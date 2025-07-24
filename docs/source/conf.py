41912bf7fbbe9c8dee979acdb8eef9f436f6be27
Eric <cheesecaketv53@gmail.com>
2025-07-25 01:44:03 +0200

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SwiftGUI'
+copyright = '2025, CheesecakeTV'
+author = 'Eric aka CheesecakeTV'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.3.0'
 
 # -- General configuration
 
